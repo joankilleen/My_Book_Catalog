09207798555333939df70a41fef76a33a7765a87
Joan Killeen <joan.killeen@bluewin.ch>
2020-05-24 15:31:04 +0100

    Added Google Books API client

---
+++ Google_Client.py
@@ -0,0 +1,46 @@
+from Book import Book
+from Book import Book_Catalog
+from typing import List
+import requests
+
+
+url_template = "https://www.googleapis.com/books/v1/volumes?q=inauthor:{}"
+class Client:
+
+	@staticmethod
+	def search_by_author(author_name):
+		url = url_template.format(author_name)
+		print(url)
+		r = requests.get(url)
+		print(f"Status Code {r.status_code} for url: {url}")
+		return r
+
+	@staticmethod
+	def get_english_titles(response):
+		num_hits = response.json()["totalItems"]
+		book_list = response.json()["items"]
+		book_hits = []
+		for item in book_list:
+			volume=item["volumeInfo"]
+			language = volume["language"]
+			isbn_13 = ""
+			if language=="en":
+				 title=volume["title"]
+				 author=volume["authors"]
+				 identifiers=volume["industryIdentifiers"]
+				 for identifier in identifiers:
+					 type=identifier["type"]
+					 if type=="ISBN_13": 
+						  isbn_13=identifier["identifier"]
+						  next_book=Book(title=title,author=author,isbn_13="isbn_13")
+						  print(f"{next_book.title} {isbn_13}")
+						  book_hits.append(next_book)
+		books_to_return=Book_Catalog(books=book_hits)
+		print(len(books_to_return.books))
+		return books_to_return
+		
+		 
+		 
+		 
+
+
--- My_Book_Catalog.py
+++ My_Book_Catalog.py
@@ -2,18 +2,16 @@ from typing import List
 from Book import Book
 from Book import Book_Catalog
 from Book import Book_State
+from Google_Client import Client
 import json
 
 BOOK_CATALOG_FILEPATH="resources/book_catalog.json"
-books=[]
-key1="1234-17"
-key2="1235-17"
-book1 = Book(title="Test1",author="Colum McCann",isbn_13=key1) 
-book2 = Book(title="Test2",author="Colum McCann",isbn_13=key2) 
-book1.state=Book_State.want_to_read
-books = Book_Catalog(books=[book1, book2])
 
-books.serialize_to_file(BOOK_CATALOG_FILEPATH)
+author="Colum McCann"
+response = Client.search_by_author(author)
+book_catalog=Client.get_english_titles(response)
+
+book_catalog.serialize_to_file(BOOK_CATALOG_FILEPATH)
 
 decoded_book_catalog = Book_Catalog.serialize_from_file(BOOK_CATALOG_FILEPATH)
 
