a0e623b20de43b9792e468b7d1434f5c3c467e4d
Joan Killeen <joan.killeen@bluewin.ch>
2020-05-24 11:56:11 +0100

    more Serialization of Book and Book_Catalog

--- Book.py
+++ Book.py
@@ -8,6 +8,30 @@ class Book:
          self.isbn_13 = isbn_13
          self.state = 'want_to_read'
 
+     @classmethod
+     def from_json(cls, data: dict):
+        return cls(**data)
+
+#List of books which can be serialized and deserializes as Json and wri^tten to file
 class Book_Catalog(object):
     def __init__(self, books: List[Book]):
         self.books = books
+
+    @classmethod
+    def from_json(cls, data: dict):
+        students = list(map(Book.from_json, data["books"]))
+        return cls(books)
+
+    # Serialize as Json and persist to file
+    def serialize_to_file(self, filepath):
+        json_object = json.dumps(self, default=lambda o: o.__dict__) 
+        with open(filepath, "w") as outfile: 
+            json.dump(json_object, outfile) 
+
+    # Rad book catalog from file
+    @staticmethod
+    def serialize_from_file(filepath):
+        with open(filepath, "r") as infile:
+            json_object = json.load(infile)
+            book_catalog = Book_Catalog(**json.loads(json_object))
+            return book_catalog
--- My_Book_Catalog.py
+++ My_Book_Catalog.py
@@ -1,16 +1,20 @@
 from Book import Book
 from Book import Book_Catalog
 from typing import List
-import json
-books=[]
 
+import json
+
+BOOK_CATALOG_FILEPATH="resources/book_catalog.json"
+books=[]
 key1="1234-17"
 key2="1235-17"
 book1 = Book(title="Test1",author="Colum McCann",isbn_13=key1) 
 book2 = Book(title="Test2",author="Colum McCann",isbn_13=key2) 
 books = Book_Catalog(books=[book1, book2])
 
- # Serializing json  
-json_object = json.dumps(books, default=lambda o: o.__dict__, indent=4) 
+books.serialize_to_file(BOOK_CATALOG_FILEPATH)
 
-print(json_object)
+decoded_book_catalog = Book_Catalog.serialize_from_file(BOOK_CATALOG_FILEPATH)
+
+print(decoded_book_catalog)
+print(decoded_book_catalog.books)
