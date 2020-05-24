95447ab6a40f7b22efc1facd9b5e396e8d11bef7
Joan Killeen <joan.killeen@bluewin.ch>
2020-05-24 12:22:57 +0100

    simple kind of enum for state

--- Book.py
+++ Book.py
@@ -1,25 +1,26 @@
 from typing import List
 import json
+import enum
 
 class Book:
      def __init__(self,title, author, isbn_13):
          self.title = title
          self.author = author
          self.isbn_13 = isbn_13
-         self.state = 'want_to_read'
+         self.state = Book_State.init
 
      @classmethod
      def from_json(cls, data: dict):
         return cls(**data)
 
-#List of books which can be serialized and deserializes as Json and wri^tten to file
+#List of books which can be serialized and deserializes as Json and written to file
 class Book_Catalog(object):
     def __init__(self, books: List[Book]):
         self.books = books
 
     @classmethod
     def from_json(cls, data: dict):
-        students = list(map(Book.from_json, data["books"]))
+        list_books = list(map(Book.from_json, data["books"]))
         return cls(books)
 
     # Serialize as Json and persist to file
@@ -35,3 +36,9 @@ class Book_Catalog(object):
             json_object = json.load(infile)
             book_catalog = Book_Catalog(**json.loads(json_object))
             return book_catalog
+
+class Book_State:
+    init=1
+    want_to_read=2
+    read=3
+    favourite=4
--- My_Book_Catalog.py
+++ My_Book_Catalog.py
@@ -1,7 +1,7 @@
+from typing import List
 from Book import Book
 from Book import Book_Catalog
-from typing import List
-
+from Book import Book_State
 import json
 
 BOOK_CATALOG_FILEPATH="resources/book_catalog.json"
@@ -10,6 +10,7 @@ key1="1234-17"
 key2="1235-17"
 book1 = Book(title="Test1",author="Colum McCann",isbn_13=key1) 
 book2 = Book(title="Test2",author="Colum McCann",isbn_13=key2) 
+book1.state=Book_State.want_to_read
 books = Book_Catalog(books=[book1, book2])
 
 books.serialize_to_file(BOOK_CATALOG_FILEPATH)
